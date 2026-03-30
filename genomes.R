282d28c153b293904fb7bfeeba0a29413381d99a
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2026-03-30 20:22:19 +0200

    Style plot outputs

--- genomes.R
+++ genomes.R
@@ -7,6 +7,8 @@ library(ggpubr)
 library(phytools)
 
 run.bash = FALSE
+run.mash = FALSE
+get.premade.tree = TRUE
 
 # pip install ncbi-genome-download
 # brew install mash
@@ -16,52 +18,59 @@ pdf = read_parquet("phenotype.parquet")
 
 ESKAPEE = c("Mycobacterium tuberculosis")
 ids = unique(pdf$assembly_ID[pdf$species %in% ESKAPEE])
-write.table(ids, file="gca_ids.txt", row.names = FALSE, quote = FALSE, col.names = FALSE)
-if(run.bash == TRUE) {
-system("bash -lc './mash-batch.sh'")
+
+if(get.premade.tree == FALSE) {
+  write.table(ids, file="gca_ids.txt", row.names = FALSE, quote = FALSE, col.names = FALSE)
+  if(run.bash == TRUE) {
+    system("bash -lc './mash-batch.sh'")
+  }
+  
+  if(run.mash == TRUE) {
+    # Mash distances -- change directory if needed
+    system("mash triangle sketches/*.msh > mash_distances.tab")
+  }
+  lines <- readLines("mash_distances.tab")
+  
+  # Number of genomes
+  n <- as.integer(lines[1])
+  
+  # Initialize distance matrix
+  dist_mat <- matrix(0, n, n)
+  
+  # Extract filenames
+  files <- character(n)
+  files[1] <- strsplit(lines[2], "/")[[1]][[4]]  # first genome
+  for(i in 2:n){
+    parts <- strsplit(lines[i + 1], "\\s+")[[1]]
+    tmp <- parts[1]
+    files[i] <- strsplit(tmp, "/")[[1]][[4]]
+  }
+  rownames(dist_mat) <- colnames(dist_mat) <- files
+  
+  # Fill lower triangle
+  # first row is zero by definition (dist to self)
+  for(i in 2:n){
+    parts <- strsplit(lines[i + 1], "\\s+")[[1]]
+    vals <- as.numeric(parts[-1])  # skip filename
+    dist_mat[i, 1:length(vals)] <- vals
+  }
+  
+  # Mirror to upper triangle
+  dist_mat[upper.tri(dist_mat)] <- t(dist_mat)[upper.tri(dist_mat)]
+  
+  # Convert to 'dist' object
+  dist_obj <- as.dist(dist_mat)
+  
+  # Build NJ tree
+  tree <- nj(dist_obj)
+  
+  # Save tree
+  write.tree(tree, paste0(ESKAPEE, "-tree.nwk", collapse=""))
+} else {
+  tree = read.tree(paste0(ESKAPEE, "-tree.nwk", collapse=""))
 }
 
-# Mash distances -- change directory if needed
-system("mash triangle sketches/*.msh > mash_distances.tab")
-lines <- readLines("mash_distances.tab")
-
-# Number of genomes
-n <- as.integer(lines[1])
-
-# Initialize distance matrix
-dist_mat <- matrix(0, n, n)
-
-# Extract filenames
-files <- character(n)
-files[1] <- strsplit(lines[2], "/")[[1]][[4]]  # first genome
-for(i in 2:n){
-  parts <- strsplit(lines[i + 1], "\\s+")[[1]]
-  tmp <- parts[1]
-  files[i] <- strsplit(tmp, "/")[[1]][[4]]
-}
-rownames(dist_mat) <- colnames(dist_mat) <- files
-
-# Fill lower triangle
-# first row is zero by definition (dist to self)
-for(i in 2:n){
-  parts <- strsplit(lines[i + 1], "\\s+")[[1]]
-  vals <- as.numeric(parts[-1])  # skip filename
-  dist_mat[i, 1:length(vals)] <- vals
-}
-
-# Mirror to upper triangle
-dist_mat[upper.tri(dist_mat)] <- t(dist_mat)[upper.tri(dist_mat)]
-
-# Convert to 'dist' object
-dist_obj <- as.dist(dist_mat)
-
-# Build NJ tree
-tree <- nj(dist_obj)
-
-# Save tree
-write.tree(tree, paste0(ESKAPEE, "-tree.nwk", collapse=""))
-
-plot(tree)
+#plot(tree)
 
 ########
 
@@ -117,17 +126,25 @@ bug = ESKAPEE[1]
 this.df = final_df[final_df$eskapee == bug,c(1,3:ncol(final_df))]
 this.df$assembly_ID = as.character(this.df$assembly_ID)
 
-plot_hyperinf_data(this.df, tree, bmargin = 80)
+plot.1 = plot_hyperinf_data(this.df, tree, bmargin = 80)
+#plot.1
 
-  fit = hyperinf(this.df, boot.parallel = 10)
-  fit.est.phy = hyperinf(this.df, auto.cluster = TRUE, boot.parallel = 10)
-  fit.phy = hyperinf(this.df, tree, boot.parallel = 10)
-  
+fit = hyperinf(this.df, boot.parallel = 10)
+fit.est.phy = hyperinf(this.df, auto.cluster = TRUE, boot.parallel = 10)
+fit.phy = hyperinf(this.df, tree, boot.parallel = 10)
+
 expt.names = c("CS", "Est phy", "Genome phy")
-plot_hyperinf_comparative(list(fit, fit.est.phy, fit.phy), expt.names=expt.names)
 fits = list(fit, fit.est.phy, fit.phy)
-plot_hyperinf_bubbles(fits, expt.names=expt.names)
 
-plot_hyperinf_ordering_matrices(list(fit, fit.est.phy, fit.phy), expt.names=expt.names ) +
+plot.2 = plot_hyperinf_comparative(fits, expt.names=expt.names)
+
+plot.3 = plot_hyperinf_bubbles(fits, expt.names=expt.names)
+
+plot.4 = plot_hyperinf_ordering_matrices(list(fit, fit.est.phy, fit.phy), expt.names=expt.names ) +
   theme(axis.text.x = element_text(angle=45, hjust=1))
+
+sf = 2
+png(paste0(ESKAPEE, "-summaries.png", collapse=""), width=800*sf, height=800*sf, res=72*sf)
+ggarrange(plot.1, plot.2, plot.3, plot.4)
+dev.off()
 
